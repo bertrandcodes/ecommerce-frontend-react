2ca791b556e7336a7f7d1f826367f9be08dbb920
Bertrand Shao <shaob@g.ucla.edu>
2020-10-18 17:56:32 -0700

    feat: add and format multiple dummy products on collection page

--- causebox-product-app/src/App.js
+++ causebox-product-app/src/App.js
@@ -34,6 +34,83 @@ class App extends React.Component {
             <div className="collection-deal">(25% off)</div>
           </div>
 
+          <div className="collection-item">
+            <div className="collection-image-container">
+              <img className="collection-image" src="https://s3-us-west-2.amazonaws.com/causebox/uploads/Ever-Eco-On-The-Go-Straw-Kit-Stainless-Steel-01.jpg">
+              </img>
+              <Unfilled />
+              <div className="collection-tag">EDITOR'S PICKS</div>
+            </div>
+            <div className="collection-brand">SOFTEN</div>
+            <div className="collection-title">Heather Bath Towel Set</div>
+            <div className="collection-prices">
+              <div className="collection-sale">$3.50</div>
+              <div className="collection-original">$5.25</div>
+            </div>
+            <div className="collection-deal">(25% off)</div>
+          </div>
+
+          <div className="collection-item">
+            <div className="collection-image-container">
+              <img className="collection-image" src="https://s3-us-west-2.amazonaws.com/causebox/uploads/Ever-Eco-On-The-Go-Straw-Kit-Stainless-Steel-01.jpg">
+              </img>
+              <Unfilled />
+              <div className="collection-tag">EDITOR'S PICKS</div>
+            </div>
+            <div className="collection-brand">SOFTEN</div>
+            <div className="collection-title">Heather Bath Towel Set</div>
+            <div className="collection-prices">
+              <div className="collection-sale">$3.50</div>
+              <div className="collection-original">$5.25</div>
+            </div>
+            <div className="collection-deal">(25% off)</div>
+          </div>
+
+          <div className="collection-item">
+            <div className="collection-image-container">
+              <img className="collection-image" src="https://s3-us-west-2.amazonaws.com/causebox/uploads/Ever-Eco-On-The-Go-Straw-Kit-Stainless-Steel-01.jpg">
+              </img>
+              <Unfilled />
+              <div className="collection-tag">EDITOR'S PICKS</div>
+            </div>
+            <div className="collection-brand">SOFTEN</div>
+            <div className="collection-title">Heather Bath Towel Set</div>
+            <div className="collection-prices">
+              <div className="collection-sale">$3.50</div>
+              <div className="collection-original">$5.25</div>
+            </div>
+            <div className="collection-deal">(25% off)</div>
+          </div>
+          <div className="collection-item">
+            <div className="collection-image-container">
+              <img className="collection-image" src="https://s3-us-west-2.amazonaws.com/causebox/uploads/Ever-Eco-On-The-Go-Straw-Kit-Stainless-Steel-01.jpg">
+              </img>
+              <Unfilled />
+              <div className="collection-tag">EDITOR'S PICKS</div>
+            </div>
+            <div className="collection-brand">SOFTEN</div>
+            <div className="collection-title">Heather Bath Towel Set</div>
+            <div className="collection-prices">
+              <div className="collection-sale">$3.50</div>
+              <div className="collection-original">$5.25</div>
+            </div>
+            <div className="collection-deal">(25% off)</div>
+          </div>
+          <div className="collection-item">
+            <div className="collection-image-container">
+              <img className="collection-image" src="https://s3-us-west-2.amazonaws.com/causebox/uploads/Ever-Eco-On-The-Go-Straw-Kit-Stainless-Steel-01.jpg">
+              </img>
+              <Unfilled />
+              <div className="collection-tag">EDITOR'S PICKS</div>
+            </div>
+            <div className="collection-brand">SOFTEN</div>
+            <div className="collection-title">Heather Bath Towel Set</div>
+            <div className="collection-prices">
+              <div className="collection-sale">$3.50</div>
+              <div className="collection-original">$5.25</div>
+            </div>
+            <div className="collection-deal">(25% off)</div>
+          </div>
         </div>
       </div>
     );
