cad19818868a07264d49cc873dbb1d1dfa1d695a
Bertrand Shao <shaob@g.ucla.edu>
2020-10-18 17:25:38 -0700

    feat: create single collection product template

--- causebox-product-app/src/App.js
+++ causebox-product-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import { ReactComponent as Cart } from './cart.svg';
+import { ReactComponent as Cart } from './assets/cart.svg';
+import { ReactComponent as Unfilled } from './assets/unfilled-heart.svg';
 
 class App extends React.Component {
   constructor() {
@@ -14,6 +15,25 @@ class App extends React.Component {
         <div className="app-header">
           <div className="logo" />
           <Cart />
+        </div>
+        <div className="collection-list">
+
+          <div className="collection-item">
+            <div className="collection-image-container">
+              <img className="collection-image" src="https://s3-us-west-2.amazonaws.com/causebox/uploads/Ever-Eco-On-The-Go-Straw-Kit-Stainless-Steel-01.jpg">
+              </img>
+              <Unfilled />
+              <div className="collection-tag">EDITOR'S PICKS</div>
+            </div>
+            <div className="collection-brand">SOFTEN</div>
+            <div className="collection-title">Heather Bath Towel Set</div>
+            <div className="collection-prices">
+              <div className="collection-sale">$3.50</div>
+              <div className="collection-original">$5.25</div>
+            </div>
+            <div className="collection-deal">(25% off)</div>
+          </div>
+
         </div>
       </div>
     );
